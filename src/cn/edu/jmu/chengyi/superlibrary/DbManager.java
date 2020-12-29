ff233c270c5e494f87d57369ce6bc01c8c207089
ksgfk <1537100607@qq.com>
2020-12-29 13:27:15 +0800

    用户增删改查

--- src/cn/edu/jmu/chengyi/superlibrary/DbManager.java
+++ src/cn/edu/jmu/chengyi/superlibrary/DbManager.java
@@ -1,6 +1,7 @@
 package cn.edu.jmu.chengyi.superlibrary;
 
 import java.math.BigDecimal;
+import java.security.MessageDigest;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -9,6 +10,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 public class DbManager {
@@ -26,10 +28,14 @@ public class DbManager {
 	public static final String TAG_PAGE = "page";
 
 	public static final String USER_TABLE = "users";
-//	public static final String TAG_
+	public static final String TAG_PWD = "password";
+	public static final String TAG_PMS = "permission";
 
 	public static final String SQL_ADD_BOOK = String.format("insert into %s (%s,%s,%s,%s,%s,%s) values (?,?,?,?,?,?)",
 			BOOK_TABLE, TAG_TYPE, TAG_NAME, TAG_COUNT, TAG_AUTHOR, TAG_PRICE, TAG_PAGE);
+
+	public static final String SQL_ADD_USER = String.format("insert into %s (%s,%s,%s) values(?,?,?)", USER_TABLE,
+			TAG_NAME, TAG_PWD, TAG_PMS);
 
 	public static String sqlRemove(String table, String tagName) {
 		return String.format("delete from %s where %s=?", table, TAG_ID);
@@ -45,6 +51,26 @@ public class DbManager {
 
 	public static String sqlUpdate(String table, String keyName) {
 		return String.format("update %s set %s=? where id=?", table, keyName);
+	}
+
+	public static String SHA1(String str) {
+		try {
+			byte[] raw = str.getBytes("utf-8");
+			MessageDigest md = MessageDigest.getInstance("SHA-1");
+			byte[] res = md.digest(raw);
+			StringBuilder sb = new StringBuilder();
+			for (byte b : res) {
+				String hex = Integer.toHexString(b & 0xFF);
+				if (hex.length() == 1) {
+					sb.append("0");
+				}
+				sb.append(hex);
+			}
+			return sb.toString();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 	private static class Instance {
@@ -63,8 +89,10 @@ public class DbManager {
 	private PreparedStatement getBookId;
 	private PreparedStatement getBookName;
 	private PreparedStatement getBookAuth;
-	
 	private PreparedStatement getUsrId;
+	private PreparedStatement getUsrName;
+	private PreparedStatement addUsr;
+	private PreparedStatement rmUserId;
 
 	private DbManager() {
 		try {
@@ -95,6 +123,21 @@ public class DbManager {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+		Clear();
+	}
+
+	private void Clear() {
+		addBook = null;
+		rmBookId = null;
+		rmBookName = null;
+		rmBookNameAuth = null;
+		getBookId = null;
+		getBookName = null;
+		getBookAuth = null;
+		getUsrId = null;
+		getUsrName = null;
+		addUsr = null;
+		rmUserId = null;
 	}
 
 	private PreparedStatement checkStatement(PreparedStatement prep, Supplier<String> getSql) throws SQLException {
@@ -204,15 +247,84 @@ public class DbManager {
 		return getBook(book.getId()).get();
 	}
 
-//	public void addUser(String name, String password) {
-//		
-//	}
-//	
-//	public boolean hasUser() {
-//		
-//	}
-//	
-//	public Optional<User> getUser(int id) {
-//		getUsrId = checkStatement(getUsrId, ()->sqlSelect());
-//	}
+	public boolean hasUser(int id) throws SQLException {
+		return getUser(id).isPresent();
+	}
+
+	public boolean hasUser(String name) throws SQLException {
+		try {
+			Optional<User> u = getUser(name);
+			return u.isPresent();
+		} catch (IllegalArgumentException e) {
+			return true;
+		}
+	}
+
+	public Optional<User> getUser(int id) throws SQLException {
+		getUsrId = checkStatement(getUsrId, () -> sqlSelect(USER_TABLE, TAG_ID));
+		getUsrId.setInt(1, id);
+		return getUser(getUsrName);
+	}
+
+	public Optional<User> getUser(String name) throws SQLException {
+		getUsrName = checkStatement(getUsrName, () -> sqlSelect(USER_TABLE, TAG_NAME));
+		getUsrName.setString(1, name);
+		ResultSet set = getUsrName.executeQuery();
+		if (!set.first())
+			return Optional.empty();
+		User u = new User(set.getInt(1), set.getString(2), set.getString(3),
+				Enum.valueOf(UserPermission.class, set.getString(4)));
+		if (set.next())
+			throw new IllegalArgumentException("repeat username:" + name + "   maybe bug");
+		return Optional.of(u);
+	}
+
+	private Optional<User> getUser(PreparedStatement sql) throws SQLException {
+		ResultSet set = sql.executeQuery();
+		if (!set.first())
+			return Optional.empty();
+		return Optional.of(new User(set.getInt(1), set.getString(2), set.getString(3),
+				Enum.valueOf(UserPermission.class, set.getString(4))));
+	}
+
+	public boolean addUser(String name, String password, Function<String, String> encry) throws SQLException {
+		if (hasUser(name))
+			return false;
+		String cry = encry.apply(password);
+		if (cry == null)
+			return false;
+		addUsr = checkStatement(addUsr, () -> SQL_ADD_USER);
+		addUsr.setString(1, name);
+		addUsr.setString(2, cry);
+		addUsr.setString(3, UserPermission.USER.name());
+		boolean result = addUsr.execute();
+		if (result) {
+			throw new IllegalStateException("unknown error");
+		}
+		return true;
+	}
+
+	public void removeUser(User user) throws SQLException {
+		User u = getUser(user.getId()).get();
+		if (u.getPermission() == UserPermission.ADMIN) {
+			throw new IllegalArgumentException("You don't have permission to remove admin");
+		}
+		rmUserId = checkStatement(rmUserId, () -> sqlRemove(USER_TABLE, TAG_ID));
+		rmUserId.setInt(1, u.getId());
+		rmUserId.execute();
+	}
+
+	public void removeUser(int id) throws SQLException {
+		Optional<User> u = getUser(id);
+		if (!u.isPresent())
+			throw new IllegalArgumentException("no user id" + id);
+		removeUser(u.get());
+	}
+
+	public void removeUser(String name) throws SQLException {
+		Optional<User> u = getUser(name);
+		if (!u.isPresent())
+			throw new IllegalArgumentException("no user id" + name);
+		removeUser(u.get());
+	}
 }
